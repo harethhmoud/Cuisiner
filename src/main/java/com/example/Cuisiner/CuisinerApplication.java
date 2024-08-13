bb8d4c3650f5c851883b4be9b6762fa5c84aa9d7
harethhmoud <hareth.hmoud@gmail.com>
2024-08-13 10:37:38 -0400

    Initial commit.

---
+++ src/main/java/com/example/Cuisiner/CuisinerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Cuisiner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuisinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuisinerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Cuisiner/CuisinerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Cuisiner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuisinerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
